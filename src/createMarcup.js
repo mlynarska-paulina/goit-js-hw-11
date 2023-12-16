80266af4ee834986e93687e48b6d328fdad8b398
Paulina Mlynarska <paulinamlynarska@outlook.com>
2023-12-16 14:03:56 +0100

    changes

---
+++ src/createMarcup.js
@@ -0,0 +1,33 @@
+export function createMarkup(arr, wraper) {
+  const markap = arr
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `<div class="photo-card">
+        <a class="link" href="${largeImageURL}"><img class ="gallary-image" src="${webformatURL}" alt="${tags}" loading="lazy" /></a>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes ${likes}</b>
+          </p>
+          <p class="info-item">
+            <b>Views ${views}</b>
+          </p>
+          <p class="info-item">
+            <b>Comments ${comments}</b>
+          </p>
+          <p class="info-item">
+            <b>Downloads ${downloads}</b>
+          </p>
+        </div>
+      </div>`
+    )
+    .join('');
+
+  wraper.insertAdjacentHTML('beforeend', markap);
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,101 @@
+import './css/style.css';
 import Notiflix from 'notiflix';
 
 import { fetchImage } from './fetchForm';
+import { createMarkup } from './createMarcup';
 
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const axios = require('axios').default;
+
+const refs = {
+  form: document.querySelector('.search-form'),
+  myInput: document.querySelector('.search-input'),
+  wraperGalery: document.querySelector('.gallery'),
+  loadMore: document.querySelector('.loadMore'),
+};
+
+const { form, myInput, wraperGalery, loadMore } = refs;
+
+let myPage = 1;
+
+form.addEventListener('submit', onSubmit);
+loadMore.addEventListener('click', addImage);
+
+const lightbox = new SimpleLightbox('.gallery a', { captionDelay: 250 });
+
+async function onSubmit(event) {
+  event.preventDefault();
+  wraperGalery.innerHTML = '';
+  myPage = 1;
+
+  const myInputValue = myInput.value;
+  const myValue = myInputValue.trim();
+
+  if (!myValue) {
+    loadMore.hidden = true;
+    return;
+  }
+
+  return await fetchThen(myValue);
+}
+
+loadMore.hidden = true;
+
+async function fetchThen(value) {
+  try {
+    const resp = await fetchImage(value);
+    const myArr = resp.data.hits;
+    const myNumber = resp.data.total;
+
+    if (myArr.length === 0) {
+      Notiflix.Notify.info(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+      loadMore.hidden = true;
+      return;
+    }
+
+    if (myNumber > 0) {
+      Notiflix.Notify.info(`Hooray! We found ${myNumber} images.`);
+    }
+
+    createMarkup(myArr, wraperGalery);
+    lightbox.refresh();
+    loadMore.hidden = false;
+
+    if (myArr.length < 20) {
+      loadMore.hidden = true;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addImage() {
+  const value2 = myInput.value;
+  let limitAdd;
+  myPage += 1;
+  try {
+    const resp = await fetchImage(value2, myPage, limitAdd);
+    createMarkup(resp.data.hits, wraperGalery);
+    onPageScrolling();
+    lightbox.refresh();
+
+    if (resp.data.hits.length < limitAdd) {
+      loadMore.hidden = true;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function onPageScrolling() {
+  const { height: cardHeight } =
+    wraperGalery.firstElementChild.getBoundingClientRect();
+  window.scrollBy({
+    top: cardHeight * 2,
+    behavior: 'smooth',
+  });
+}
